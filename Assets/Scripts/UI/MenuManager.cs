1ac9f094f66bf6a8057d998314f2b4d5f15eca9c
BezerkMangos <Sattaur04@gmail.com>
2023-10-07 21:15:52 -0400

    UI
    Pause menu and Un-Pause menu were added and are functional, the game stops time when the button is pressed and resumes when it is pressed again.

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,23 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
+
+    //Load Screen Game Objects
     public GameObject loadingScene;
     public Slider slider;
 
+    //Pause Screen Game Objects
+    public GameObject PausePanel;
+    public GameObject PauseButton;
+    public GameObject UnPauseButton;
 
-    public void ToGame()
+
+    private void Awake()
     {
-        StartCoroutine(LoadAsynchronously());
+        PausePanel.SetActive(false);
     }
 
-    IEnumerator LoadAsynchronously()
+
+    #region Scene Swap method
+    //This method will take an input of a scene name then take you to that scene
+    public void ToScene(string scene)
     {
-        AsyncOperation operation = SceneManager.LoadSceneAsync("SampleScene");
+        StartCoroutine(LoadAsynchronously(scene));
+    }
+    #endregion
+
+
+    #region LoadingScreen Method
+    //This scene can be called to pull up the loading screen while loading a scene called from the "ToScene" method
+    IEnumerator LoadAsynchronously(string scene)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(scene);
 
         while (!operation.isDone)
         {
@@ -31,7 +51,29 @@ public class MenuManager : MonoBehaviour
             yield return null;
         }
     }
+    #endregion
 
+
+    #region Pause Game Method
+    //Pause the game bet setting the time scale to 0 so all time activites 
+    //Activates the pause menu pannel to pull up a new UI
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+        PauseButton.SetActive(false);
+    }
+    #endregion
+
+
+    #region Un Pause Method
+    public void UnPauseGame()
+    {
+        Time.timeScale = 1;
+        PausePanel.SetActive(false);
+        PauseButton.SetActive(true);
+    }
+    #endregion
 
 
 
